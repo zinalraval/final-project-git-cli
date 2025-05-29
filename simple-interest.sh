427f69f8a6ed6aecbcaa18b953fa784801a3faf2
zinalraval <zinal.software@gmail.com>
2025-05-29 10:20:30 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+# Formula: SI = (P × R × T) / 100
+
+echo "Enter Principal:"
+read P
+echo "Enter Rate of Interest:"
+read R
+echo "Enter Time (in years):"
+read T
+
+SI=$((P * R * T / 100))
+
+echo "Simple Interest is: $SI"
 
